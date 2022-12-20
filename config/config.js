96d3ef82f090f6a7e5a66978967eafae602b9131
Vishvesh a.k.a Str1d3r <vishveshsrao@gmail.com>
2022-12-20 14:12:33 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
+// All values below are for testing purposes please change them according to your use case and scenerio
+
 export const RPC_URl = 'https://rpc.ankr.com/eth_goerli';
 export const TOKEN_CONTRACT_ADDRESS = '0x7af963cF6D228E564e2A0aA0DdBF06210B38615D';
 export const PORT = 5000;
-// export const START_BLOCK = 8133614;
-// export const END_BLOCK = 8134896;
-export const START_BLOCK = 8164522;
-export const END_BLOCK = 8164542;
+export const ITERATIONS = 4;  
+export const END_BLOCK_OFFSET = 8165542;   // put 'latest' to search till latest block
+export const START_BLOCK_OFFSET = END_BLOCK_OFFSET - 3000;
